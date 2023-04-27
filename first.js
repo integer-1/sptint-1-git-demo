9b5bdc07bc08f6a76f2f69429baa7fd2de1349e1
Suwon Jeong <integer.won@gmail.com>
2023-04-28 10:40:44 +1200

    second commit

--- first.js
+++ first.js
@@ -1,18 +1,17 @@
+let userlist = [
+  { name: 'Mike', age: 30 },
+  { name: 'Jane', age: 27 },
+  { name: 'Tom', age: 10 },
+]
 
-let  userlist = [
-    { name : 'Mike', age : 30},
-    { name : 'Jane', age : 27},
-    { name : 'Tom', age : 10},
-    
-];
-
-let newUserlist = userlist.map(
-    function(user,index) {
-    return Object.assign({}, user, {
-        id : index +1,
-        isAdult : user.age >19,
-    })
+let newUserlist = userlist.map(function (user, index) {
+  return Object.assign({}, user, {
+    id: index + 1,
+    isAdult: user.age > 19,
+  })
 })
 
 console.log(newUserlist)
 console.log(userlist)
+
+//code changed
